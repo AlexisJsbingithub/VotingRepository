767222a59719e475455175174bbff1ede721adc5
AlexisJsbingithub <alexis.mendoza.perso@gmail.com>
2022-04-23 10:36:07 +0200

    Updates

---
+++ precache-manifest.54713c78e595e4ea57735006b8c91d52.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71fc15954c53941b74b078e042f44fd",
+    "url": "/VotingRepository/index.html"
+  },
+  {
+    "revision": "45cf7365520bc557e889",
+    "url": "/VotingRepository/static/css/2.37c457eb.chunk.css"
+  },
+  {
+    "revision": "6fff4ec5f3ce8a5e2ea7",
+    "url": "/VotingRepository/static/css/main.1383865a.chunk.css"
+  },
+  {
+    "revision": "45cf7365520bc557e889",
+    "url": "/VotingRepository/static/js/2.7e3c99ba.chunk.js"
+  },
+  {
+    "revision": "6fff4ec5f3ce8a5e2ea7",
+    "url": "/VotingRepository/static/js/main.64dbb8b3.chunk.js"
+  },
+  {
+    "revision": "da030158050fab400060",
+    "url": "/VotingRepository/static/js/runtime-main.6ab9cae1.js"
+  },
+  {
+    "revision": "37482ed36a1f2e46043b64158ec034c2",
+    "url": "/VotingRepository/static/media/NotVoter.37482ed3.png"
+  },
+  {
+    "revision": "da01d1c49c37615dc46600241ebb3c34",
+    "url": "/VotingRepository/static/media/RegisteringProposals.da01d1c4.png"
+  },
+  {
+    "revision": "3a192f61d0166b0bf1f265896b699a75",
+    "url": "/VotingRepository/static/media/RegisteringVoters.3a192f61.png"
+  },
+  {
+    "revision": "1471be1c1b9cec7a0b60e215d3ad6f7b",
+    "url": "/VotingRepository/static/media/VotingSession.1471be1c.png"
+  },
+  {
+    "revision": "4655f74878bc2dff9bc3625d1dc6dcf5",
+    "url": "/VotingRepository/static/media/WinningProposal.4655f748.png"
+  }
+]);
